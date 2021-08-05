502e5d9410545cb3b4d3a37570f21f433859b52e
JasmineGuy <guyjasmine.c@gmail.com>
2021-08-05 17:19:04 -0500

    tweaks

--- starter/main.js
+++ starter/main.js
@@ -113,4 +113,6 @@ switch (language){
         console.log('Great language too! :D')
 }
 
-// Conditional Operator
+// Conditional Ternary Operator
+
+console.log(`${country}'s population is ${population > 33000000 ? 'above' : 'below'} average`);
